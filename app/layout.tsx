1651f89049f4abf178298517679b93de5967a9ef
PopaGeorge123 <popageo02@gmail.com>
2026-04-09 09:38:43 +0300

    dev : added meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -79,6 +79,30 @@ export default function RootLayout({
             gtag('config', 'G-YYVDYMF75Q');
           `,
         }} />
+
+        <Script id="facebook-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2543614049387480');
+            fbq('track', 'PageView');
+          `,
+        }} />
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img height="1" width="1" style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=2543614049387480&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
+
       </head>
       <body className="min-h-full flex flex-col bg-[#13131f]">
         <PostHogProvider>
