9f465caeb8bc8a96396c532e3831956baca512ba
PopaGeorge123 <popageo02@gmail.com>
2026-05-18 13:37:28 +0300

    dev : added meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,6 +104,30 @@ export default function RootLayout({
         </Script>
 
 
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2543614049387480');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img height="1" width="1" style={{display:"none"}}
+            src="https://www.facebook.com/tr?id=2543614049387480&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel */}
+
         <Script type="text/javascript">
         {`
         var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
